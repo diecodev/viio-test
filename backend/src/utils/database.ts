b567298fdd982e7358e24e078364430885366068
diecodev <diegodiaz05@gmail.com>
2023-11-16 21:42:22 -0500

    Set up mongo connection

---
+++ backend/src/utils/database.ts
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+import { Products, Users } from "../types";
+
+const DB_URL = "mongodb://mongo:27017";
+const DB_NAME = "viio-challenge";
+const USER_COLLECTION = "users";
+const PRODUCTS_COLLECTION = "products";
+
+const client = new MongoClient(DB_URL);
+
+const connectDatabase = async () => {
+  await client.connect();
+  console.log("Connected successfully to database server");
+
+  return client.db(DB_NAME);
+};
+
+const db = await connectDatabase();
+
+const users = db?.collection<Users>(USER_COLLECTION);
+const products = db?.collection<Products>(PRODUCTS_COLLECTION);
+
+export { users, products, db };
