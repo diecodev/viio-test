d6d9cd05a79aa5992e72554b5662e5a88ddef43d
diecodev <diegodiaz05@gmail.com>
2023-11-16 23:44:01 -0500

    Remove swagger from main app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,17 @@
-// env vars
-import "dotenv/config";
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
+// env vars
+import "dotenv/config";
+import { env } from "hono/adapter";
+import type { EnvVariables } from "./types";
 // middlewares
 import { jwt } from "hono/jwt";
 import { prettyJSON } from "hono/pretty-json";
 import { timing } from "hono/timing";
 import { logger } from "hono/logger";
-import { swaggerUI } from "@hono/swagger-ui";
 // routers
 import { router as userRouter } from "./routes/user";
-import { env } from "hono/adapter";
-import { EnvVariables } from "./types";
+import { router as productsRouter } from "./routes/products";
 
 const app = new Hono();
 
@@ -30,9 +30,9 @@ app.use("^(?!/user/sign-in$).*", async (c, next) => {
 
   return await auth(c, next);
 });
-app.get("/ui", swaggerUI({ url: "/doc" }));
 
 app.route("/user", userRouter);
+app.route("/products", productsRouter);
 
 serve(
   {
