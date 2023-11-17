fa38a979d329b57bce1159f2ce1214dbf4e14801
diecodev <diegodiaz05@gmail.com>
2023-11-16 21:42:46 -0500

    Setup db and env vars types

---
+++ backend/src/types.ts
@@ -0,0 +1,23 @@
+// DB TYPES
+export type Users = {
+  email: string;
+  password: string;
+};
+
+export type Products = {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  total: number;
+  discountPercentage: number;
+  discountedPrice: number;
+  thumbnail: string;
+};
+// END DB TYPES
+
+// ENV VARS TYPES
+export type EnvVariables = {
+  JWT_SECRET: string;
+  JWT_COOKIE: string;
+};
